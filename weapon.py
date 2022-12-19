7c12a2b1f24febf781c9604cc65cc0e7e00dd198
ncrector <kaesh831@gmail.com>
2022-12-19 00:01:33 -0800

    more changes

--- weapon.py
+++ weapon.py
@@ -3,6 +3,15 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self,player,groups):
         super().__init__(groups)
-        self.image = pygame.Surface((20,20))
-        self.rect = self.image.get_rect(center = player.rect.center)
         self.image_depth = 1
+        direction = player.status.split('_'[0])
+        
+
+        # graphics
+        self.image = pygame.Surface((20,20))
+
+        # placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft = player.rect.midright)
+        else:
+            self.rect = self.image.get_rect(center = player.rect.center)
